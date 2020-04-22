323aaf119e151d9f8672f86b32f6e789b5c38395
kateryna_kornysheva <katbackdev@gmail.com>
2020-04-22 12:37:13 +0300

    TDO integration in proccess

---
+++ src/DTO/Request/RequestDTOInterface.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\DTO\Request;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface RequestDTOInterface
+{
+    /**
+     * RequestDTOInterface constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request);
+}
---
+++ src/Resolver/RequestDTOResolver.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace App\Resolver;
+
+use App\DTO\Request\RequestDTOInterface;
+use App\Util\ValidatorAwareTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class RequestDTOResolver implements ArgumentValueResolverInterface
+{
+    use ValidatorAwareTrait;
+
+    /**
+     * @param Request $request
+     * @param ArgumentMetadata $argument
+     * @return bool
+     */
+    public function supports(Request $request, ArgumentMetadata $argument)
+    {
+        try {
+            $classImplements = class_implements($argument->getType());
+            return $classImplements !== false && in_array(RequestDTOInterface::class, array_values($classImplements));
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param ArgumentMetadata $argument
+     * @return \Generator
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): \Generator
+    {
+        $class = $argument->getType();
+        $dto = new $class($request);
+        $this->validator->validate($dto);
+
+        yield $dto;
+    }
+}
